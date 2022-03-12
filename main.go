c0e34b3b89b36187501dfd9db5a1b62307f419e3
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-12 23:45:42 +0700

    set: refactoring

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	chain := blockchain.GetBlockchain()
-	fmt.Println(chain)
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
