6e72367b3ff881d5fd8a55b752a4442270780540
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-13 15:49:22 +0700

    set: newServeMux

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -5,11 +5,11 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
 	"github.com/plusbeauxjours/GO-crypto/blockchain"
 )
 
 const (
-	port        string = ":4000"
 	templateDir string = "explorer/templates/"
 )
 
@@ -37,11 +37,12 @@ func add(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Start() {
+func Start(port int) {
+	handler := http.NewServeMux()
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	handler.HandleFunc("/", home)
+	handler.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handler))
 }
