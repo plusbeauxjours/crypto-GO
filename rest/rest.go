c40d11866206ebe8183c1d45f85c91263933d7f9
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-19 22:17:12 +0700

    set: coinbase transaction

--- rest/rest.go
+++ rest/rest.go
@@ -28,9 +28,6 @@ type urlDescription struct {
 	Payload     string `json:"payload,omitempty"`
 }
 
-type addBlockBody struct {
-	Message string
-}
 
 type errorResponse struct {
 	ErrorMessage string `json:"errorMessage"`
@@ -73,9 +70,7 @@ func blocks(rw http.ResponseWriter, r *http.Request) {
 	case "GET":
 		utils.HandleErr(json.NewEncoder(rw).Encode(blockchain.Blockchain().Blocks()))
 	case "POST":
-		var addBlockBody addBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		blockchain.Blockchain().AddBlock(addBlockBody.Message)
+		blockchain.Blockchain().AddBlock()
 		rw.WriteHeader(http.StatusCreated)
 	}
 }
